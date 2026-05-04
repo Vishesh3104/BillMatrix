4355cfeeac4a7ccf402bc3d554d2a5f28b104600
Vishesh3104 <120164446+Vishesh3104@users.noreply.github.com>
2026-05-04 19:25:04 +0530

    Add files via upload

---
+++ Hubs/NotificationHub.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace BILLMATRIX1.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public async Task JoinUserGroup(string userId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, $"user_{userId}");
+        }
+    }
+}
